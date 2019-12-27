e9ddf2e315c03e7001889a1782b405a58b5ef1ba
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2019-12-27 23:28:23 +0200

    added numbers animation and poem reset by hover order

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
 let words
-let hoveredWords = 0
+let newWords = []
 
 async function getMivzakim() {
   let response = await fetch('/mivzak');
@@ -12,16 +12,16 @@ async function getMivzakim() {
 function renderWords(words, url) {
   let poemString = ''
   for (let i=0; i<words.length; i++) {
-    let word=words[i]
+    let word=words[i].text
 
     let classesStr = ''
     if (['ממשלה','ישראל','כנסת'].some(function(v) { return word.indexOf(v) >= 0; })) {
-      classesStr = ' poop'
+      classesStr = 'poop'
     }
     if (word.search(/\d/) !== -1) {
-      console.log('word: ' + word + ' has a number');
+      classesStr = 'number'
     }
-    poemString += `<span class="word word${i}${classesStr} hidden-word" onmouseover="hoveredWord(this)">${word+' '}</span>`
+    poemString += `<span class="word ${classesStr} hidden-word" data-index="${i}" onmouseover="hoveredWord(this)">${word+' '}</span>`
     if (Math.random() > 0.725) {
       poemString += '\n'
     }
@@ -39,17 +39,30 @@ function renderWords(words, url) {
   setTimeout(function () {
     appearifyWords()
   }, 100);
-  return Promise.resolve();
 }
 
-function cleanArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    let idx = array[i].search(/[A-Z, a-z, א-ת, 0-9]/)
-    if (idx < 0) {
-      array.splice(i, 1)
+function formatWords(words) {
+  let wordObjs = []
+  for (var i = words.length - 1; i > 0; i--) {
+    let idx = words[i].search(/[A-Z, a-z, א-ת, 0-9]/)
+    if (idx < 0) continue
+    let wordObj = {
+      text: words[i],
     }
+    wordObjs.push(wordObj)
   }
-  return Promise.resolve()
+  return wordObjs
+}
+
+function randomizeWordAttribute() {
+  for (var i=0; i < words.length; i++) {
+    let word = words[i]
+    word.scale = twoDigits(Math.random()*0.9 + 0.6)
+    word.rotateZ = twoDigits(Math.random()*30 - 15)
+    word.translateX = twoDigits(Math.random()*50 - 25)
+    word.translateY = twoDigits(Math.random()*20 - 10)
+    word.transition = twoDigits(Math.random() * 1100 + 100)
+  }
 }
 
 function shuffleArray(array) {
@@ -59,7 +72,6 @@ function shuffleArray(array) {
     array[i] = array[j];
     array[j] = temp;
   }
-  return Promise.resolve()
 }
 
 function createRandomStyle() {
@@ -72,12 +84,12 @@ function createRandomStyle() {
   }
   let cssString = ''
   for (var i=0; i < words.length; i++) {
-    cssString += `.word${i} {
-      transform: scale(${ twoDigits(Math.random()*0.9 + 0.6) })
-                 rotateZ(${ twoDigits(Math.random()*30 - 15) }deg)
-                 translateX(${ twoDigits(Math.random()*50 - 25) }px)
-                 translateY(${ twoDigits(Math.random()*20 - 10) }px) !important;
-      transition: ${ twoDigits(Math.random() * 1100 + 100) }ms;
+    cssString += `.word[data-index='${i}'] {
+      transform: scale(${ words[i].scale })
+                 rotateZ(${ words[i].rotateZ }deg)
+                 translateX(${ words[i].translateX }px)
+                 translateY(${ words[i].translateY }px) !important;
+      transition: ${ words[i].transition }ms;
     }
     `
   }
@@ -90,31 +102,53 @@ function appearifyWords() {
     for (let i=0; i< wordElms.length ; i++) {
       setTimeout(function (elm) {
         elm.classList.remove("hidden-word");
-      }, 60 * i, wordElms[i]);
+      }, (Math.random()*20 + 40) * i, wordElms[i]);
     }
 }
 
 function hoveredWord(wordElm) {
-  if (wordElm.className.includes('hovered')) return
-  wordElm.className += ' hovered';
-  hoveredWords++
-  if (hoveredWords === words.length) {
+  if (wordElm.classList.contains('hovered')) return
+  wordElm.classList.add('hovered');
+  newWords.push(words[wordElm.dataset.index])
+  if (newWords.length === words.length) {
     // reset or something
+
+    words = newWords
+    newWords = []
+    setTimeout(function () {
+      renderWords(words)
+    }, 200);
   } else {
     setTimeout(function () {
+      randomizeWordAttribute()
       createRandomStyle()
     }, 100);
   }
 }
 
+function recursiveAnimateNumbers() {
+  let numberElms = document.getElementsByClassName("number");
+  for (var i = 0; i < numberElms.length; i++) {
+    setTimeout(function (elm) {
+      let digits = elm.innerText.match(/\d+/)[0].length
+      elm.innerText = elm.innerText.replace(/\d+/, Math.floor(Math.random()*Math.pow(10, digits)+(10^(digits-1))))
+    }, Math.floor(Math.random()*900+100), numberElms[i]);
+  }
+  setTimeout(function () {
+    recursiveAnimateNumbers()
+  }, 150);
+}
+
 async function run() {
   let result = await getMivzakim()
-  words = result.words
-  await cleanArray(words)
-  await shuffleArray(words)
+  words = await formatWords(result.words)
+  // await cleanArray(words)
+  randomizeWordAttribute()
+  shuffleArray(words)
   console.log(words);
-  await renderWords(words)
-  await createRandomStyle()
+  renderWords(words)
+  createRandomStyle()
+  recursiveAnimateNumbers()
 }
 
 function twoDigits(num) {
