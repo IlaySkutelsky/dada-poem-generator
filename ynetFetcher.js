eb6670e1188950063c66269d8d098ed3b3774418
IlaySkutelsky <40234996+IlaySkutelsky@users.noreply.github.com>
2020-11-25 23:59:27 +0200

    simplified ynet fetcher

--- ynetFetcher.js
+++ ynetFetcher.js
@@ -8,29 +8,26 @@ const mivzakHeaderSelector = "h1.art_header_title"
 const mivzakBodySelector = "div.art_body div span p"
 
 async function scrapMivzak() {
-  return axios.get(mivzakimURL).then((response) => {
-      const $ = cheerio.load(response.data)
-      let firstMivzak = $(firstMivzakSelector)
-      let firstMivzakURL = "https://www.ynet.co.il/" + $(firstMivzak[0]).attr('href')
-      // console.log("axios firstMivzakURL:");
-      // console.log(firstMivzakURL);
-      return axios.get(firstMivzakURL).then((response) => {
-        const $ = cheerio.load(response.data)
-        let mivzakHeaderText = $(mivzakHeaderSelector).contents().first().text();
-        let mivzakBodyText = $(mivzakBodySelector).contents().first().text();
-        let mivzakString = mivzakHeaderText + ' ' + mivzakBodyText
-        // console.log("axios mivzakString:");
-        // console.log(mivzakString);
-        return {
-          text: mivzakString,
-          url: firstMivzakURL
-        }
-      })
-  })
+  let mivzakimResponse = await axios.get(mivzakimURL)
+  let $ = cheerio.load(mivzakimResponse.data)
+  let firstMivzak = $(firstMivzakSelector)
+  let firstMivzakURL = "https://www.ynet.co.il/" + $(firstMivzak[0]).attr('href')
+  let firstMivzakresponse = await axios.get(firstMivzakURL)
+  // console.log("axios firstMivzakURL:");
+  // console.log(firstMivzakURL);
+  $ = cheerio.load(firstMivzakresponse.data)
+  let mivzakHeaderText = $(mivzakHeaderSelector).contents().first().text();
+  let mivzakBodyText = $(mivzakBodySelector).contents().first().text();
+  let mivzakString = mivzakHeaderText + ' ' + mivzakBodyText
+  // console.log("axios mivzakString:");
+  // console.log(mivzakString);
+  return {
+    text: mivzakString,
+    url: firstMivzakURL
+  }
 };
 
 ynetFetcher = function() {
-
   this.fetchMivzak = async function(cb) {
     let mivzak = await scrapMivzak()
     cb(null, mivzak);
