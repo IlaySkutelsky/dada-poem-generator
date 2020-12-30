c12c3ed98422a8280a531bb16f8e1b4054a1b413
IlaySkutelsky <40234996+IlaySkutelsky@users.noreply.github.com>
2020-12-30 09:28:21 +0200

    new ynet structure fixes

--- ynetFetcher.js
+++ ynetFetcher.js
@@ -2,25 +2,21 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-const mivzakimURL = "https://www.ynet.co.il/home/0,7340,L-184,00.html"
-const firstMivzakSelector = "td.ghciMivzakimHeadlines1 table tbody tr:nth-child(2) a"
-const mivzakHeaderSelector = "h1.art_header_title"
-const mivzakBodySelector = "div.art_body div span p"
+const mivzakimURL = "https://www.ynet.co.il/news/category/184"
+const firstMivzakSelector = ".slotContentDiv .slotsContent .slotList .slotView:nth-child(1) a"
+const mivzakHeaderSelector = "h1.mainTitle"
+const mivzakBodySelector = ".textEditor_container span[data-text=true]"
 
 async function scrapMivzak() {
   let mivzakimResponse = await axios.get(mivzakimURL)
   let $ = cheerio.load(mivzakimResponse.data)
   let firstMivzak = $(firstMivzakSelector)
-  let firstMivzakURL = "https://www.ynet.co.il/" + $(firstMivzak[0]).attr('href')
+  let firstMivzakURL = $(firstMivzak[0]).attr('href')
   let firstMivzakresponse = await axios.get(firstMivzakURL)
-  // console.log("axios firstMivzakURL:");
-  // console.log(firstMivzakURL);
   $ = cheerio.load(firstMivzakresponse.data)
   let mivzakHeaderText = $(mivzakHeaderSelector).contents().first().text();
   let mivzakBodyText = $(mivzakBodySelector).contents().first().text();
   let mivzakString = mivzakHeaderText + ' ' + mivzakBodyText
-  // console.log("axios mivzakString:");
-  // console.log(mivzakString);
   return {
     text: mivzakString,
     url: firstMivzakURL
