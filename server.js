a241526ea16022079967a1dff8f3462b78e4361f
Ilay Skutelsky <ilay.skutelsky@gmail.com>
2023-02-21 23:21:26 +0200

    small lines switch

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const port = process.env.PORT || 3000
 const wallaFetcherService = require('./wallaFetcher').wallaFetcher;
 const wallaFetcher = new wallaFetcherService();
 
-app.use(express.static('dist'))
-app.use('original', express.static('dist/original'))
-
 app.get('/mivzak', function(req, res) {
   wallaFetcher.fetchMivzak(function(error, mivzak) {
     res.set('Cache-Control', 'no-store')
     res.send(mivzak);
   });
 });
+
+app.use(express.static('dist'))
 
 app.listen(port, () => console.log(`Dada-poem-generator server listening on port ${port}!`))
